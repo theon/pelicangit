50fca8ea966f8785382966374c6de2f6cf0b459f
Ian Forsey <forsey@gmail.com>
2012-04-15 20:33:49 +0000

    Added setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+requires = ['pelican']
+
+try:
+    import argparse
+except ImportError:
+    requires.append('argparse')
+
+entry_points = {
+    'console_scripts': [
+        'pelicangit = pelicangit:main'
+   ]
+}
+
+setup(
+    name = "pelicangit",
+    version = "0.1",
+    url = 'http://theon.github.com/pelicangit',
+    author = 'Ian Forsey',
+    author_email = 'forsey@gmail.com',
+    description = "",
+    long_description=open('README.md').read(),
+    packages = ['pelicangit'],
+    include_package_data = True,
+    install_requires = requires,
+    entry_points = entry_points,
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: GNU Affero General Public License v3',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+)
